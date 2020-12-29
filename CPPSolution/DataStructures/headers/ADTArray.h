9f47f308ef13a9c3db955dcdff73e14477ba2b0d
chi_nik <chi_nik@msn.com>
2020-12-29 22:31:09 +0100

    Add Resize and Append

--- CPPSolution/DataStructures/headers/ADTArray.h
+++ CPPSolution/DataStructures/headers/ADTArray.h
@@ -9,8 +9,8 @@ private:
 	//const size_type _Size=4;
 #define ARRAY_SIZE 4
 
-	size_type length;
-	size_type size;
+	size_type length; // what is the last element of the array
+	size_type size; // how large is the array in memory
 	T* Array;
 
 		
@@ -19,10 +19,47 @@ public:
 	{
 		Array = new T[ARRAY_SIZE];
 		size = ARRAY_SIZE;
-		length = ARRAY_SIZE;
+		length = 0;
+		Append(1);
+		Append(2);
+		Append(3);
 
 	}
 	//~ADTArray();
+	void Append(T elem)
+	{
+		if (length < size)
+		{
+			Array[length++] = elem; 
+		}
+		else
+		{
+			Resize(size + 1);
+			Append(elem);
+
+		}
+	}
+	void Resize(size_type new_size) 
+	{
+		// allocate, then copy, delete
+		if (new_size > size)
+		{ 
+			std::cout << "Growing the array to" << new_size << std::endl;
+
+			auto ta = new T[new_size];
+			for (auto i = 0; i < length; ++i)
+			{
+				ta[i] = Array[i]; 
+			}
+			delete[] Array;
+			Array = ta;
+			size = new_size;
+			ta = nullptr; 
+		}
+		// TODO: Implement when new_size < size
+
+
+	}
 
 	void Display()
 	{
--- CPPSolution/DataStructures/src/DataStructures.cpp
+++ CPPSolution/DataStructures/src/DataStructures.cpp
@@ -4,6 +4,10 @@ int main()
 {
 	ADTArray a1;
 	a1.Display();
+	a1.Append(4);
+	a1.Append(5);
+	a1.Append(6);
+	a1.Display();
     std::cout << "Hello World!\n"; 
 }
 
