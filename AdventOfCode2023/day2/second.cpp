50632f4543e24ab64e711e18649183af6e06d92b
chi_nik <chi_nik@msn.com>
2024-01-14 23:37:01 +0100

    [AoC] Day2: finished 2 parts

--- AdventOfCode2023/day2/second.cpp
+++ AdventOfCode2023/day2/second.cpp
@@ -25,6 +25,10 @@ struct Draw {
 			}
 		}
 	}
+
+	bool isBusted(int l_r, int l_g, int l_b){
+		return (r > l_r) || (g > l_g) || (b > l_b) ;
+	}
 	friend ostream& operator<<(ostream& s, Draw d);
 	
 };
@@ -35,13 +39,73 @@ ostream& operator<<(ostream& s, Draw d){
 struct GameEntry {
 	int gameNum;
 	vector<Draw> vd;
+	GameEntry(string gs){
+		// extract gameid
+		smatch sm;
+		regex_match(gs,sm, regex("Game (\\d+):.*"));
+		gameNum=stoi(sm[1]);
+		gs.erase(gs.begin(), gs.begin()+gs.find(':')+1);
+		while(gs.find(';') != string::npos){
+			string substr= gs.substr(0, gs.find(';')+1);
+			gs.erase(gs.begin(), gs.begin()+gs.find(';')+1);
+			vd.push_back(Draw(substr));
+		}
+		vd.push_back(Draw(gs));
+		cout << gs << endl; 
+	
+	}
+	bool isGameBusted(){
+		for(auto i: vd){
+			if ( i.isBusted(12,13,14)) return true;
+		}
+		return false;
+	}
+	int getPower() {
+		int r_max, g_max, b_max;
+		r_max = vd[0].r;
+		g_max = vd[0].g;
+		b_max = vd[0].b;
+		for ( auto i : vd) {
+			r_max = (i.r > r_max) ? i.r : r_max;
+			g_max = (i.g > g_max) ? i.g : g_max;
+			b_max = (i.b > b_max) ? i.b : b_max;
+		}
+
+		return r_max*b_max*g_max;
+	}
+	friend ostream& operator<<(ostream& s, GameEntry ge);
+	
 
 };
 
+ostream& operator<<(ostream& s, GameEntry ge)
+{
+	s << "G: " << ge.gameNum << endl;
+	for( auto i: ge.vd){
+		s << i << endl;
+	}
+	return s;
+}
 int main(){
 	vector<string> data = file_reader("input.txt");
-	Draw a(" 209 green, 3 blue, 23 red ;");
-	cout << a <<endl;
+	cout << data[0] ;
+	int result_gameid_sum=0;
+	int result_gamepower_sum=0;
+	for(auto d : data){
+		GameEntry ge(d);
+		if(ge.isGameBusted()) {
+			cout << "BUSTEDGAME";
+		}
+		else{
+			result_gameid_sum += ge.gameNum;
+		}
+		result_gamepower_sum += ge.getPower();
+
+		cout << ge;
+
+	}
+	cout << result_gameid_sum << endl;
+	cout << result_gamepower_sum << endl;
 
 
 
